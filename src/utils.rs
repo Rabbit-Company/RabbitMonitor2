951e63cb98edb966b8638252d229580ec0c037a2
Ziga <ziga.zajc007@gmail.com>
2023-04-23 12:48:51 +0200

    Update

--- src/utils.rs
+++ src/utils.rs
@@ -12,36 +12,35 @@ fn create_metric(name: impl Display, description: impl Display, value: impl Disp
 
 pub fn create_metrics(monitor: Arc<Mutex<Monitor>>) -> String{
 	let mut metrics: String = String::from("");
-	let temp: MutexGuard<Monitor> = monitor.lock().unwrap();
+	{
+		let temp: MutexGuard<Monitor> = monitor.lock().unwrap();
 
-	if temp.settings.logger >= 1 {
-		metrics += &create_metric("cpu_load_1min", "CPU load recorded in last minute", temp.processor.min1.to_string());
-		metrics += &create_metric("cpu_load_5min", "CPU load recorded in last 5 minutes", temp.processor.min5.to_string());
-		metrics += &create_metric("cpu_load_15min", "CPU load recorded in last 15 minutes", temp.processor.min15.to_string());
+		if temp.settings.logger >= 1 {
+			metrics += &create_metric("cpu_load_1min", "CPU load recorded in last minute", temp.processor.min1.to_string());
+			metrics += &create_metric("cpu_load_5min", "CPU load recorded in last 5 minutes", temp.processor.min5.to_string());
+			metrics += &create_metric("cpu_load_15min", "CPU load recorded in last 15 minutes", temp.processor.min15.to_string());
 
-		metrics += &create_metric("memory_total", "Total memory in bytes", temp.memory.total.to_string());
-		metrics += &create_metric("memory_available", "Available memory in bytes", temp.memory.available.to_string());
-		metrics += &create_metric("memory_used", "Used memory in bytes", temp.memory.used.to_string());
-		metrics += &create_metric("memory_free", "Free memory in bytes", temp.memory.free.to_string());
+			metrics += &create_metric("memory_total", "Total memory in bytes", temp.memory.total.to_string());
+			metrics += &create_metric("memory_available", "Available memory in bytes", temp.memory.available.to_string());
+			metrics += &create_metric("memory_used", "Used memory in bytes", temp.memory.used.to_string());
+			metrics += &create_metric("memory_free", "Free memory in bytes", temp.memory.free.to_string());
 
-		metrics += &create_metric("swap_total", "Total swap storage in bytes", temp.swap.total.to_string());
-		metrics += &create_metric("swap_used", "Used swap storage in bytes", temp.swap.used.to_string());
-		metrics += &create_metric("swap_free", "Free swap storage in bytes", temp.swap.free.to_string());
+			metrics += &create_metric("swap_total", "Total swap storage in bytes", temp.swap.total.to_string());
+			metrics += &create_metric("swap_used", "Used swap storage in bytes", temp.swap.used.to_string());
+			metrics += &create_metric("swap_free", "Free swap storage in bytes", temp.swap.free.to_string());
 
-		metrics += &create_metric("storage_total", "Total storage in bytes", temp.storage.total.to_string());
-		metrics += &create_metric("storage_used", "Used storage in bytes", temp.storage.used.to_string());
-		metrics += &create_metric("storage_free", "Free storage in bytes", temp.storage.free.to_string());
+			metrics += &create_metric("storage_total", "Total storage in bytes", temp.storage.total.to_string());
+			metrics += &create_metric("storage_used", "Used storage in bytes", temp.storage.used.to_string());
+			metrics += &create_metric("storage_free", "Free storage in bytes", temp.storage.free.to_string());
+		}
+
+		metrics += &create_metric("cpu_load_percent", "CPU load in percent", format!("{:.2}", temp.processor.percent));
+		metrics += &create_metric("memory_percent", "Used memory in percent", format!("{:.2}", temp.memory.percent));
+		metrics += &create_metric("swap_percent", "Used swap storage in percent", format!("{:.2}", temp.swap.percent));
+		metrics += &create_metric("storage_percent", "Used storage in percent", format!("{:.2}", temp.storage.percent));
+		metrics += &create_metric("network_download", "Download speed in bytes", temp.network.download.to_string());
+		metrics += &create_metric("network_upload", "Upload speed in bytes", temp.network.upload.to_string());
 	}
-
-  metrics += &create_metric("cpu_load_percent", "CPU load in percent", format!("{:.2}", temp.processor.percent));
-	metrics += &create_metric("memory_percent", "Used memory in percent", format!("{:.2}", temp.memory.percent));
-  metrics += &create_metric("swap_percent", "Used swap storage in percent", format!("{:.2}", temp.swap.percent));
-  metrics += &create_metric("storage_percent", "Used storage in percent", format!("{:.2}", temp.storage.percent));
-	metrics += &create_metric("network_download", "Download speed in bytes", temp.network.download.to_string());
-	metrics += &create_metric("network_upload", "Upload speed in bytes", temp.network.upload.to_string());
-
-	drop(temp);
-
 	metrics
 }
 
